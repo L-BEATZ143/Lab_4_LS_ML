4414a2b13e7bf6e75de4506bec1b28ea15fe1a77
Morgan Lancaster <paleblood03@gmail.com>
2026-09-23 12:08:45 -0500

    added setw(10) and left alignment to every line

--- main.cpp
+++ main.cpp
@@ -7,22 +7,21 @@ int main() {
     int itemQuantity;
     double unitPrice;
     bool memberCheck;
-    
-    std::cout << "Enter food name: ";
+ 
+    std::cout << std::setw(10) << std::left << "Enter food name: ";
     std::getline(std::cin, foodName);
 
-    std::cout << "Enter item code: ";
-    std::cin >> itemCode;
+    std::cout << std::setw(10) << std::left << "Enter item code: ";
+    std::cin >> std::setw(10) >> std::left >> itemCode;
 
-    std::cout << "Enter quantity: ";
-    std::cin >> itemQuantity;
+    std::cout << std::setw(10) << std::left << "Enter quantity: ";
+    std::cin >> std::setw(10) >> std::left >> itemQuantity;
 
-    std::cout << "Enter unit price: ";
-    std::cin >> unitPrice;
+    std::cout << std::setw(10) << std::left << std::setprecision(2) << "Enter unit price: ";
+    std::cin >> std::setw(10) >> std::left >> unitPrice;
 
-    std::cout << "Are you a member?: ";
-    std::cin >> memberCheck;
+    std::cout << std::setw(10) << std::left << "Are you a member?: ";
+    std::cin >> std::setw(10) >> std::left >> memberCheck;
 
 //pushinput again
-
 }
