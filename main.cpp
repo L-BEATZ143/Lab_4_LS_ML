d379b9badb4b9ef9f91c9684368ad24596d74097
paleblood03 <paleblood03@gmail.com>
2026-09-24 23:15:03 -0500

    feat: Add member discount logic and audit table

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     char itemCode;
     int itemQuantity;
     double unitPrice;
-    bool memberCheck;
+    char memberCheck;
 
     std::cout << std::setw(10) << std::left << "Enter food name: " << std::endl;
     std::getline(std::cin, foodName);
@@ -22,18 +22,39 @@ int main() {
     std::cout << std::setw(10) << std::left << "Enter unit price: " << std::endl;
     std::cin >> unitPrice;
 
-    std::cout << std::setw(10) << std::left << "Are you a member?: " << std::endl;
+    std::cout << std::setw(10) << std::left << "Are you a member? (y/n): " << std::endl;
     std::cin >> memberCheck;
 
     std::cin.ignore();
     std::cout << std::setw(10) << std::left << "Enter cashier notes: " << std::endl;
     std::getline(std::cin, cashierNotes);
 
+    double totalCost = itemQuantity * unitPrice;
 
-    if (memberCheck = true) { 
-        auto totalCost = itemQuantity * unitPrice * 0.9;
+     if (memberCheck == 'y' || memberCheck == 'Y') {
+        totalCost = totalCost * 0.90;
     }
-    else {
-        auto totalCost = itemQuantity * unitPrice * 0.9;
-    }
+     std::cout << "\n----- RECEIPT -----\n";
+     std::cout << std::left << std::setw(15) << "item"
+         << std::setw(10) << "Code"
+         << std::setw(10) << "Qty"
+         << std::setw(10) << "Price" << std::endl;
+     std::cout << std::left << std::setw(15) << foodName
+         << std::setw(10) << itemCode
+         << std::setw(10) << itemQuantity
+         << std::fixed << std::setprecision(2) << unitPrice << std::endl;
+
+     std::cout << "Total: $" << std::fixed << std::setprecision(2) << totalCost << std::endl;
+
+     std::cout << "\n----- INVENTORY AUDIT -----\n";
+
+     std::cout << std::left
+         << std::setw(15) << "Item"
+         << std::setw(10) << "Code"
+         << std::right << std::setw(10) << "Quantity" << std::endl;
+
+     std::cout << std::left
+         << std::setw(15) << foodName
+         << std::setw(10) << itemCode
+         << std::right << std::setw(10) << itemQuantity << std::endl;
 }
